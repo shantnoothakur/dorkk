c6b5cef1d96181c9e987bda44c0232ee1eb51331
shantnoothakur <shantnooshandilya@gmail.com>
2025-09-21 16:44:19 +0530

    Update banner_anim.py

--- advaitzz/banner_anim.py
+++ advaitzz/banner_anim.py
@@ -1,16 +1,22 @@
-import time
 import sys
+import time
 
-BANNER = r"""
-    ___    ____  __      __    ___   ___ ___  ____
-   /   |  / __ \/ /     / /   /   | /   |__ \/ __ \
-  / /| | / / / / /     / /   / /| |/ /| |_/ / / / /
- / ___ |/ /_/ / /___  / /___/ ___ / ___ |/ /_/ / 
-/_/  |_/_____ /_____/ /_____/_/  |_/_/  |_/_____/  
+ASCII_BANNER = r"""
+    ___  ____  __      __    _ _______ ___ _______ 
+   / _ \/ __ \/ /     / /   (_) ___/ // / ___/ / 
+  / , _/ /_/ / /__   / /__ / / /__/ _  / /__/ /  
+ /_/|_|\____/____/  /____//_/\___/_//_/\___/_/   
+                                                  
+               ADVAITZZ - Dork & Recon Tool
 """
 
-def print_banner(delay=0.02):
-    for line in BANNER.splitlines():
-        print(line)
-        time.sleep(delay)
-    print("\n")
+def print_banner():
+    print(ASCII_BANNER)
+
+def spinner(duration=1.5, message="Loading"):
+    for _ in range(int(duration/0.1)):
+        for sym in "|/-\\":
+            sys.stdout.write(f"\r{message}... {sym}")
+            sys.stdout.flush()
+            time.sleep(0.1)
+    sys.stdout.write("\r")
